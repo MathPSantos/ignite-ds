df117dc6af0ebe1eb08f176efe02996334c9b197
MathPSantos <matheus.psantos2016@gmail.com>
2023-03-05 00:15:17 -0300

    Initialize repo

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Heading, HeadingProps } from "@ignite-ui/react";
+
+export default {
+  title: "Typography/Heading",
+  component: Heading,
+  args: {
+    children: "Custom title",
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: `Por padrão o heading será um 'h2'`,
+      },
+    },
+  },
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj<HeadingProps> = {};
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: "H1 Heading",
+    as: "h1",
+  },
+};
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Text, TextProps } from "@ignite-ui/react";
+
+export default {
+  title: "Typography/Text",
+  component: Text,
+  args: {
+    children: "Apenas um texto",
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    as: "strong",
+  },
+};
